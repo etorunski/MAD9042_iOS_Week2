f9556e236f9710ae65bddeab827b1047e82bc4e8
Eric Torunski <etorunski@erics-macbook-pro.local>
2018-08-29 12:53:11 -0400

    Initial Commit

---
+++ TextXCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextXCode
+//
+//  Created by Eric Torunski on 2018-08-29.
+//  Copyright © 2018 Eric Torunski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
