8dc959bdec69ca42fcd94ac3ac7202bccb482f76
Eric Torunski <etorunski@gmail.com>
2019-01-22 20:07:46 -0500

    Initial commit

---
+++ TextXCode/PageTwoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PageTwoViewController.swift
+//  TextXCode
+//
+//  Created by Eric Torunski on 2018-11-06.
+//  Copyright © 2018 Eric Torunski. All rights reserved.
+//
+
+import UIKit
+
+class PageTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clicky(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TextXCode/ViewController.swift
+++ TextXCode/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import CoreMotion
+import AVFoundation
 
 class ViewController: UIViewController {
 
+   /* This touchUpInside handler turns on / off the flashlight */
+    @IBAction func buttonWasClicked(_ sender: Any) {
+        
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        
+        do {
+            try device.lockForConfiguration()
+            
+            if (device.torchMode == AVCaptureDevice.TorchMode.on) {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            } else {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
+    @IBOutlet weak var thirdPageButton: UIButton!
+    
+    
+    let motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let myArray = NSMutableArray()
+        myArray.add("Hello")
+        
+        
+        
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.1
+            motionManager.startAccelerometerUpdates(to: OperationQueue.main) { (data, error) in
+//                data?.acceleration.x
+                print(data)
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
